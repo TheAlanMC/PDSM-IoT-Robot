ca43ccc4d4e96d96f545be1b9841c745ee4f4a49
Radomir Vladislavić <vladislavic.rl@gmail.com>
2023-06-08 16:27:55 -0400

    add components folder

--- angular-iot-serverless-robot/src/app/app-routing.module.ts
+++ angular-iot-serverless-robot/src/app/app-routing.module.ts
@@ -1,9 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { MatchesComponent } from './matches/matches.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import {HomeComponent} from "./components/home/home.component";
+import {RobotsComponent} from "./components/robots/robots.component";
+import {RoomsComponent} from "./components/rooms/rooms.component";
+import {RoomHomeComponent} from "./components/room-home/room-home.component";
 
-const routes: Routes = [{ path: 'matches', component: MatchesComponent }];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'robots', component: RobotsComponent },
+  { path: 'rooms', component: RoomsComponent },
+  { path: 'rooms/:id', component: RoomHomeComponent },
+  { path: 'matches', component: MatchesComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angular-iot-serverless-robot/src/app/app.module.ts
+++ angular-iot-serverless-robot/src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MatchesComponent } from './matches/matches.component';
+import { MatchesComponent } from './components/matches/matches.component';
+import { HomeComponent } from './components/home/home.component';
+import { RobotsComponent } from './components/robots/robots.component';
+import { RoomsComponent } from './components/rooms/rooms.component';
+import { RoomHomeComponent } from './components/room-home/room-home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MatchesComponent
+    MatchesComponent,
+    HomeComponent,
+    RobotsComponent,
+    RoomsComponent,
+    RoomHomeComponent
   ],
   imports: [
     BrowserModule,
